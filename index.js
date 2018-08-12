7a64f7e8c1a13ba84636b8bc0e7e79a298196d11
James palakkal <jvpalakkal@gmail.com>
2018-08-12 05:37:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ theBeatlesPlay(musicians, instruments)
 function theBeatlesPlay(musicians, instruments) {
   var newArr = []
   for(var i =0; i < musicians.length; i++) {
-    newArr.push(musicians[i] + " plays " + instruments[i]);
+    newArr.push(musicians[i] + " plays " + instruments[i])
   }
-  return newArr;
+  return newArr
 }
