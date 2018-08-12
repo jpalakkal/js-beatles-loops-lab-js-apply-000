f6231330e297990d7b2423188b472cb6134eaf4f
James palakkal <jvpalakkal@gmail.com>
2018-08-12 05:35:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 
-theBeatlesPlay(musicians, instruments); 
+theBeatlesPlay(musicians, instruments)
 
 function theBeatlesPlay(musicians, instruments) {
   var newArr = []; 
